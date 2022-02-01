ca86ca4e058fc25b06c06f2af3d85b9e2bdfb583
Marc Klein <marc.klein1989@gmail.com>
2022-02-01 12:03:44 +0100

    feat: added styled mui Paper

--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -2,28 +2,36 @@ import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
+import { experimentalStyled as styled } from "@mui/material/styles";
+
+const Item = styled(Paper)(({ theme }) => ({
+	...theme.typography.body2,
+	padding: theme.spacing(2),
+	textAlign: "center",
+	color: theme.palette.text.secondary,
+}));
 
 const Menu = () => {
 	return (
 		<Box sx={{ flexGrow: 1 }} alignItems="center">
 			<Grid container spacing={1} columns={{ xs: 2 }}>
 				<Grid item xs={6}>
-					<Paper>Im a Menu point</Paper>
+					<Item>Im a Menu point</Item>
 				</Grid>
 				<Grid item xs={6}>
-					<Paper>Im a Menu point</Paper>
+					<Item>Im a Menu point</Item>
 				</Grid>
 				<Grid item xs={6}>
-					<Paper>Im a Menu point</Paper>
+					<Item>Im a Menu point</Item>
 				</Grid>
 				<Grid item xs={6}>
-					<Paper>Im a Menu point</Paper>
+					<Item>Im a Menu point</Item>
 				</Grid>
 				<Grid item xs={4}>
-					<Paper>Im a Menu point</Paper>
+					<Item>Im a Menu point</Item>
 				</Grid>
 				<Grid item xs={4}>
-					<Paper>Im a Menu point</Paper>
+					<Item>Im a Menu point</Item>
 				</Grid>
 			</Grid>
 		</Box>
