9f041732ca47185232b408002f6a3dc5fd732e84
Marc Klein <marc.klein1989@gmail.com>
2022-02-01 14:12:56 +0100

    feat: added Link und foo test for routing

---
+++ src/pages/foo.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Pages = () => {
+	return <div>hello</div>;
+};
+export default Pages;
--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -3,6 +3,7 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import { experimentalStyled as styled } from "@mui/material/styles";
+import Link from "next/link";
 
 const Item = styled(Paper)(({ theme }) => ({
 	...theme.typography.body2,
@@ -16,22 +17,32 @@ const Menu = () => {
 		<Box sx={{ flexGrow: 1 }} alignItems="center">
 			<Grid container spacing={1} columns={{ xs: 2 }}>
 				<Grid item xs={6}>
-					<Item>Im a Menu point</Item>
-				</Grid>
-				<Grid item xs={6}>
-					<Item>Im a Menu point</Item>
-				</Grid>
-				<Grid item xs={6}>
-					<Item>Im a Menu point</Item>
-				</Grid>
-				<Grid item xs={6}>
-					<Item>Im a Menu point</Item>
+					<Item>
+						<Link href="/">Im a Menupoint</Link>
+					</Item>
 				</Grid>
 				<Grid item xs={4}>
-					<Item>Im a Menu point</Item>
+					<Link href="/">Im a Menupoint</Link>
 				</Grid>
 				<Grid item xs={4}>
-					<Item>Im a Menu point</Item>
+					<Item>
+						<Link href="/">Im a Menupoint</Link>
+					</Item>
+				</Grid>
+				<Grid item xs={4}>
+					<Item>
+						<Link href="/">Im a Menupoint</Link>
+					</Item>
+				</Grid>
+				<Grid item xs={4}>
+					<Item>
+						<Link href="/">Im a Menupoint</Link>
+					</Item>
+				</Grid>
+				<Grid item xs={4}>
+					<Item>
+						<Link href="/">Im a Menupoint</Link>
+					</Item>
 				</Grid>
 			</Grid>
 		</Box>
