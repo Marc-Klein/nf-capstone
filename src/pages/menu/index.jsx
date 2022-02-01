f74100e569c1d54c9a511938b0ccefcbc43b2ae4
Marc Klein <marc.klein1989@gmail.com>
2022-02-01 11:26:10 +0100

    feat: first Menuu with mui

--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,2 +1,33 @@
-import React from "react";
-import
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+
+const Menu = () => {
+	return (
+		<Box sx={{ flexGrow: 1 }} alignItems="center">
+			<Grid container spacing={1} columns={{ xs: 2 }}>
+				<Grid item xs={6}>
+					<Paper>Im a Menu point</Paper>
+				</Grid>
+				<Grid item xs={6}>
+					<Paper>Im a Menu point</Paper>
+				</Grid>
+				<Grid item xs={6}>
+					<Paper>Im a Menu point</Paper>
+				</Grid>
+				<Grid item xs={6}>
+					<Paper>Im a Menu point</Paper>
+				</Grid>
+				<Grid item xs={4}>
+					<Paper>Im a Menu point</Paper>
+				</Grid>
+				<Grid item xs={4}>
+					<Paper>Im a Menu point</Paper>
+				</Grid>
+			</Grid>
+		</Box>
+	);
+};
+
+export default Menu;
