c68ff16975f1ac8bbd5c327cc1119a067447feea
Marc Klein <marc.klein1989@gmail.com>
2022-02-03 11:06:41 +0100

    chore: added leaflat

---
+++ src/organisms/store/index.jsx
@@ -0,0 +1,16 @@
+// import create from "zustand";
+// import axios from "axios";
+//
+// const fetchable = {
+// 	loading: false,
+// 	data: null,
+// 	error: null,
+// };
+//
+// const initializeFetchable = () => ({ ...fetchable });
+//
+// const useStore = create((set, get))=> ({
+// 	location: initializeFetchable(),
+// 	route: initializeFetchable(),
+// 	nearbyPlaces: initializeFetchable(),
+// })
