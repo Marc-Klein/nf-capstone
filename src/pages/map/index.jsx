12b2ace041535df700913307944d2d6bf61417bb
Marc Klein <marc.klein1989@gmail.com>
2022-02-07 12:44:07 +0100

    feat: deleted Custom Box import

--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import BottomNav from "../../molecules/bottom-nav";
-import CustomBox from "../../atoms/styled-Box";
 
 const NoSsrMap = dynamic(() => import("../../atoms/initialize-map"), { ssr: false });
 
