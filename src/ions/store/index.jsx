70190a6ada46ce57c14adecdd949f026dbbab9c8
Marc Klein <marc.klein1989@gmail.com>
2022-02-08 17:05:30 +0100

    feat: added Lon and Lat in store

--- src/ions/store/index.jsx
+++ src/ions/store/index.jsx
@@ -2,5 +2,25 @@ import create from "zustand";
 import { persist } from "zustand/middleware";
 import produce from "immer";
 
-const useStore = create(persist(set)=>)
+const useStore = create(
+	persist(set => {
+		return {
+			position: [],
+			addLatitude: latitude => {
+				set(
+					produce(state => {
+						state.navigator.geolocation.getCurrentPosition(latitude);
+					})
+				);
+			},
+			addLongitude: longitude => {
+				set(
+					produce(state => {
+						state.navigator.geolocation.getCurrentPosition(longitude);
+					})
+				);
+			},
+		};
+	})
+);
 export default useStore;
