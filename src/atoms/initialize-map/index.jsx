df8f0fe7643fdbd223f08cd6c665ad9d95f8424b
Marc Klein <marc.klein1989@gmail.com>
2022-02-10 09:32:48 +0100

    feat: added lat and long

--- src/atoms/initialize-map/index.jsx
+++ src/atoms/initialize-map/index.jsx
@@ -19,6 +19,7 @@ const Map = ({ poll = 1_000_000 }) => {
 		};
 	}, [poll]);
 	useEffect(() => {
+		const latitude = currentPosition ? currentPosition.coords.latitude : undefined;
 		console.log(
 			currentPosition ? currentPosition.coords.latitude : "getting latitude",
 			currentPosition ? currentPosition.coords.longitude : "getting longitude"
@@ -26,7 +27,14 @@ const Map = ({ poll = 1_000_000 }) => {
 	}, [currentPosition]);
 
 	return (
-		<StyledMapContainer center={[47.909, 7.0]} zoom={[16]} scrollWheelZoom={false}>
+		<StyledMapContainer
+			center={[
+				currentPosition ? currentPosition.coords.latitude : "getting latitude",
+				currentPosition ? currentPosition.coords.longitude : "getting longitude",
+			]}
+			zoom={[16]}
+			scrollWheelZoom={false}
+		>
 			<TileLayer
 				url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFrbDg5IiwiYSI6ImNrejg4NHY3azA2djcybm8xdmx4M2xpdHcifQ.ftANL_cw0hp3m_NUP-CeoQ"
 				attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
