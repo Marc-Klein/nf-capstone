35d5e5275dea487bff7197084ffc8f41a12a6a1d
Marc Klein <marc.klein1989@gmail.com>
2022-02-11 10:32:11 +0100

    chore: fixed location tracking

--- src/atoms/initialize-map/index.jsx
+++ src/atoms/initialize-map/index.jsx
@@ -1,36 +1,14 @@
-import React, { useEffect } from "react";
-import { TileLayer, Marker, Popup } from "react-leaflet";
+import React from "react";
+import { TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import StyledMapContainer from "./styled.js";
-import useStore from "../../ions/store";
+import Location from "../get-position";
 
-const Map = ({ poll = 1_000_000 }) => {
-	const currentPosition = useStore(state => state.currentPosition);
-
-	useEffect(() => {
-		const setCurrentPosition = useStore.getState().setCurrentPosition;
-		const interval = setInterval(() => {
-			setCurrentPosition();
-		}, poll);
-		setCurrentPosition();
-		return () => {
-			clearInterval(interval);
-		};
-	}, [poll]);
-	useEffect(() => {
-		console.log(
-			currentPosition ? currentPosition.coords.latitude : "getting latitude",
-			currentPosition ? currentPosition.coords.longitude : "getting longitude"
-		);
-	}, [currentPosition]);
-
+const Map = () => {
 	return (
 		<StyledMapContainer
-			center={[
-				currentPosition ? currentPosition.coords.latitude : null,
-				currentPosition ? currentPosition.coords.longitude : null,
-			]}
+			center={{ lat: 49.233334, lng: 7.0 }}
 			zoom={[16]}
 			scrollWheelZoom={false}
 		>
@@ -38,9 +16,7 @@ const Map = ({ poll = 1_000_000 }) => {
 				url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFrbDg5IiwiYSI6ImNrejg4NHY3azA2djcybm8xdmx4M2xpdHcifQ.ftANL_cw0hp3m_NUP-CeoQ"
 				zoomControl={false}
 			/>
-			<Marker draggable animate position={[49.233334, 7.0]}>
-				<Popup>Here I am</Popup>
-			</Marker>
+			<Location />
 		</StyledMapContainer>
 	);
 };
