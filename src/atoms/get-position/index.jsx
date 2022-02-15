4c3b770891c99c4326e0c71270d1e93985b81548
Marc Klein <marc.klein1989@gmail.com>
2022-02-15 09:28:35 +0100

    feat: added svg to map, creds to Lenni

--- src/atoms/get-position/index.jsx
+++ src/atoms/get-position/index.jsx
@@ -5,12 +5,19 @@ import { useMapEvents, Marker, Popup } from "react-leaflet";
 // import UserIcon from "./human-location.png";
 // import markerIconPng from "leaflet/dist/images/human-location.png";
 
-const GetIcon = _iconSize => {
-	return L.icon({
-		iconUrl: "human-location.png",
-		iconSize: _iconSize,
-	});
-};
+const markerIcon = L.divIcon({
+	html: `
+         <svg
+           width="100"
+           height="100"
+           viewBox="0 0 100 100"
+         >
+          <path fill="currentColor" d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
+         </svg>`,
+	className: "",
+	iconSize: [48, 48],
+	iconAnchor: [12, 40],
+});
 
 //maybe where the map gets rendered
 const Location = () => {
@@ -29,7 +36,7 @@ const Location = () => {
 		console.log(event);
 	});
 	return position === null ? null : (
-		<Marker position={position} icon={GetIcon(20)}>
+		<Marker animate position={position} icon={markerIcon}>
 			<Popup>You are here!</Popup>
 		</Marker>
 	);
