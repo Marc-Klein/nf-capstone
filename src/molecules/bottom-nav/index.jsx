6fb831dc8dae3045a49213f86d22ef4fe97c5480
Marc Klein <marc.klein1989@gmail.com>
2022-02-17 09:38:34 +0100

    feat: added more Styles for on map Button

--- src/molecules/bottom-nav/index.jsx
+++ src/molecules/bottom-nav/index.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
 import ReportGmailerrorredIcon from "@mui/icons-material/ReportGmailerrorred";
 import ReportIcon from "@mui/icons-material/Report";
 import ShieldIcon from "@mui/icons-material/Shield";
 import HomeIcon from "@mui/icons-material/Home";
-
-// import Location from "../../atoms/get-position";
 
 const BottomNav = () => {
 	const [value, setValue] = React.useState("");
 	const handleChange = () => (event, newValue) => {
 		setValue(newValue);
 	};
-	//Todo how to pass locate method to get position
-	// const locateMe= ()=> {
-	// 	return useMapEvents(locateMe.locate()}
 
 	return (
 		<BottomNavigation sx={{ width: "100%" }} value={value} onChange={handleChange}>
@@ -28,7 +22,6 @@ const BottomNav = () => {
 			/>
 			<BottomNavigationAction label="Safe" value="safe" icon={<ShieldIcon />} />
 			<BottomNavigationAction label="Home" value="home" icon={<HomeIcon />} />
-			<BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
 		</BottomNavigation>
 	);
 };
