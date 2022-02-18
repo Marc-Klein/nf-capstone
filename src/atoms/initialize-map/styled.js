a6bbfbdd3111e02479106b2825d95ae781404105
Marc Klein <marc.klein1989@gmail.com>
2022-02-18 17:32:59 +0100

    chore: put cmt outside of css const

--- src/atoms/initialize-map/styled.js
+++ src/atoms/initialize-map/styled.js
@@ -1,10 +1,10 @@
 import styled from "@emotion/styled";
 import { MapContainer } from "react-leaflet";
 
+//substrates the height of the appbar and bottom bar from 100vh
 const StyledMapContainer = styled(MapContainer)`
 	position: relative;
 	width: 100%;
-	//substrates the height of the appbar and bottombar from 100vh
 	height: calc(100vh - 112px);
 	margin: 0;
 	padding: 0;
