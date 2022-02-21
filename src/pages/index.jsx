5ef726eeb4bc5a2309c1d38e4f1ef6e6a7deb104
Marc Klein <marc.klein1989@gmail.com>
2022-02-21 09:25:00 +0100

    feat: max width of 400px for login, linting

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,8 +20,8 @@ const StyledDiv = styled.div`
 	transform: translate(-50%, -50%);
 	border-radius: 2em;
 	background: #fff;
-	box-shadow: 0 0 15px -1px #000;
 	max-width: 400px;
+	box-shadow: 0 0 15px -1px #000;
 `;
 
 const StyledBox = styled(Box)`
