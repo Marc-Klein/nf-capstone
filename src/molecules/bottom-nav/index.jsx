df57afe2cffae6eae6f9d599c8eaeb1bcd0fcc10
Marc Klein <marc.klein1989@gmail.com>
2022-02-23 18:52:55 +0100

    feat: added snackbars to buttons

--- src/molecules/bottom-nav/index.jsx
+++ src/molecules/bottom-nav/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import ReportGmailerrorredIcon from "@mui/icons-material/ReportGmailerrorred";
@@ -6,6 +6,9 @@ import ChatIcon from "@mui/icons-material/Chat";
 import ShieldIcon from "@mui/icons-material/Shield";
 import HomeIcon from "@mui/icons-material/Home";
 import Dialog from "@mui/material/Dialog";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+
 import Chat from "../../organisms/chat";
 
 // const buttonMessages = {
@@ -16,8 +19,11 @@ import Chat from "../../organisms/chat";
 // };
 
 const BottomNav = () => {
-	const [value, setValue] = React.useState("");
-	const [open, setOpen] = React.useState(false);
+	const [value, setValue] = useState("");
+	const [open, setOpen] = useState(false);
+	const [snackbarRequest, setSnackbarRequest] = useState(false);
+	const [snackbarSafe, setSnackbarSafe] = useState(false);
+	const [snackbarHome, setSnackbarHome] = useState(false);
 
 	const handleChange = () => (event, newValue) => {
 		setValue(newValue);
@@ -33,10 +39,62 @@ const BottomNav = () => {
 			>
 				<Chat />
 			</Dialog>
+			<Snackbar
+				autoHideDuration={6000}
+				open={snackbarRequest}
+				onClose={() => {
+					setSnackbarRequest(false);
+				}}
+			>
+				<Alert
+					severity="warning"
+					sx={{ width: "100%" }}
+					onClose={() => {
+						setSnackbarRequest(false);
+					}}
+				>
+					Request has been sent
+				</Alert>
+			</Snackbar>
+
+			<Snackbar
+				autoHideDuration={6000}
+				open={snackbarSafe}
+				onClose={() => {
+					setSnackbarSafe(false);
+				}}
+			>
+				<Alert
+					severity="info"
+					sx={{ width: "100%" }}
+					onClose={() => {
+						setSnackbarSafe(false);
+					}}
+				>
+					I am safe
+				</Alert>
+			</Snackbar>
+			<Snackbar
+				autoHideDuration={6000}
+				open={snackbarHome}
+				onClose={() => {
+					setSnackbarHome(false);
+				}}
+			>
+				<Alert
+					severity="success"
+					sx={{ width: "100%" }}
+					onClose={() => {
+						setSnackbarHome(false);
+					}}
+				>
+					I am home
+				</Alert>
+			</Snackbar>
 			<BottomNavigation value={value} onChange={handleChange}>
 				<BottomNavigationAction
-					label="Danger"
-					value="danger"
+					label="Chat"
+					value="chat"
 					icon={<ChatIcon />}
 					onClick={() => {
 						setOpen(true);
@@ -46,9 +104,26 @@ const BottomNav = () => {
 					label="Aware"
 					value="aware"
 					icon={<ReportGmailerrorredIcon />}
+					onClick={() => {
+						setSnackbarRequest(true);
+					}}
 				/>
-				<BottomNavigationAction label="Safe" value="safe" icon={<ShieldIcon />} />
-				<BottomNavigationAction label="Home" value="home" icon={<HomeIcon />} />
+				<BottomNavigationAction
+					label="Safe"
+					value="safe"
+					icon={<ShieldIcon />}
+					onClick={() => {
+						setSnackbarSafe(true);
+					}}
+				/>
+				<BottomNavigationAction
+					label="Home"
+					value="home"
+					icon={<HomeIcon />}
+					onClick={() => {
+						setSnackbarHome(true);
+					}}
+				/>
 			</BottomNavigation>
 		</>
 	);
