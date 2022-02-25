dfb53688c862ecfc72062e0092866735a48ca949
Marc Klein <marc.klein1989@gmail.com>
2022-02-25 16:09:58 +0100

    chore: tried out new pwa settings

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -21,6 +21,21 @@ const App = ({ Component, pageProps }) => {
 		<SessionProvider session={pageProps.session}>
 			<PubNubProvider client={pubnub}>
 				<Theme>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link crossOrigin rel="preconnect" href="https://fonts.gstatic.com" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Stencil+Text&family=Caveat&family=Comfortaa:wght@400;700&family=Josefin+Sans:wght@400;700&family=Nunito:wght@400;700&family=Open+Sans:wght@400;500;600;700&family=Sansita+Swashed&display=swap"
+						rel="stylesheet"
+					/>
+					<meta name="application-name" content="EatMe" />
+					<meta name="apple-mobile-web-app-capable" content="yes" />
+					<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+					<meta name="apple-mobile-web-app-title" content="EatMe" />
+					<meta name="format-detection" content="telephone=no" />
+					<meta name="mobile-web-app-capable" content="yes" />
+					<meta name="robots" content="noindex,nofollow" />
+					<meta name="theme-color" content="#226F54" />
+					<link rel="manifest" href="../../public/manifest/manifest.json" />
 					<CssBaseline />
 
 					<Component {...pageProps} />
